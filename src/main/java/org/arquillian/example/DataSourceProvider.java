3696cd77bc7a6e7b79fc502ca73626effc99d07f
marioBSKT <sestinidario@gmail.com>
2021-12-17 12:19:47 +0100

    [WIP] broken test in an embedded glassfish runtime

---
+++ src/main/java/org/arquillian/example/DataSourceProvider.java
@@ -0,0 +1,23 @@
+package org.arquillian.example;
+
+import org.apache.camel.cdi.ContextName;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import javax.inject.Named;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+@ApplicationScoped
+@ContextName("dummy-context")
+public class DataSourceProvider {
+
+    @Produces
+    @Named("datasource")
+    public javax.sql.DataSource getDataSource() throws Exception {
+        InitialContext ctx = new InitialContext();
+        DataSource ds = (DataSource) ctx.lookup("java:jboss/ExampleDataSource");
+        return ds;
+    }
+
+}
